dafb28bf0fbd311a46ecc9d79ded4479f6a14a09
rashmishrm <rashmishrm74@gmail.com>
2017-04-18 15:39:51 -0700

    Adding error steps

--- receiver-docker/startup_script.sh
+++ receiver-docker/startup_script.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
+echo "step 1) starting ssh"
+service sshd start || { echo "ssh failed"; exit 1; }
 
-service sshd start
-service cassandra start
 
+echo "step 2) starting cassandra"
+service cassandra start|| { echo "cassandra start failed"; exit 1; }
+
+echo "step 3) starting zookeeper"
 su pipeline-admin $HOME/kafka/bin/zookeeper-server-start.sh $HOME/kafka/config/zookeeper.properties  > /home/pipeline-admin/zookeeper.log 2>&1 &
+
+echo "step 4) starting kafka server"
 su pipeline-admin $HOME/kafka/bin/kafka-server-start.sh $HOME/kafka/config/server.properties > /home/pipeline-admin/kafka.log 2>&1 &
 
+
+echo "step 5) Waiting before cassandra table creation"
 sleep 18
-cqlsh -f init_cassandra.cql
+echo Creating cassandra tables
+cqlsh -f init_cassandra.cql|| { echo "Cassandra data creation failed"; exit 1; }
+
 
 sleep 5
-echo starting notebook
+echo "step 6) starting notebook"
 nohup jupyter notebook --ip='*' &
 
-echo starting flume agent
+echo "step 7) starting flume Receiver agent"
 cd $HOME/flume/bin
 nohup ./flume-ng agent --conf /home/pipeline-admin/flume/conf --conf-file /home/pipeline-admin/flume_receive.conf --name receiveAgent -Df.root.logger=INFO,console &
--- source-docker/start-flume.sh
+++ source-docker/start-flume.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 ip=$1
 echo $ip
-sed -i 's/<Reciever-IP>/'$ip'/g' /home/pipeline-admin/flume_send.conf
 
-echo starting flume agent
-cd $HOME/flume/bin
+echo "step 1) copying ip at flume_send.conf"
+
+sed -i 's/<Reciever-IP>/'$ip'/g' /home/pipeline-admin/flume_send.conf || { echo "IP coudnt be copied."; exit 1; }
+
+echo "step 2) starting flume agent"
+cd $HOME/flume/bin || { echo "flume/bin folder is not present"; exit 1; }
 nohup ./flume-ng agent --conf /home/pipeline-admin/flume/conf --conf-file /home/pipeline-admin/flume_send.conf --name sendAgent -Df.root.logger=INFO,console &
